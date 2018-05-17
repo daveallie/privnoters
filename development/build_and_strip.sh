0f8bf5b143432ec7e0044d2cb9ddd78c24d3078a
Dave Allie <dave@daveallie.com>
2018-05-17 23:20:43 +1000

    Small release bins and build script for mac & linux

---
+++ development/build_and_strip.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname $0)"
+cd ..
+
+case `uname -s` in
+    Linux)
+        FULL_TOOLCHAIN="x86_64-unknown-linux-gnu"
+        ;;
+    Darwin)
+        FULL_TOOLCHAIN="x86_64-apple-darwin"
+        ;;
+    *)
+        FULL_TOOLCHAIN="$(rustup toolchain list | grep default | awk '{print $1}' | cut -d '-' -f2-)"
+        ;;
+esac
+
+RELEASE_FOLDER="./target/${FULL_TOOLCHAIN}/release"
+BINARY_PATH="${RELEASE_FOLDER}/privnoters"
+ZIP_FILE="privnoters-$(git rev-parse --short HEAD)-${FULL_TOOLCHAIN}.tar.gz"
+ZIP_PATH="${RELEASE_FOLDER}/${ZIP_FILE}"
+FINAL_ZIP_PATH="./pkg/${ZIP_FILE}"
+
+rm ${BINARY_PATH} &>/dev/null || true
+rm ${ZIP_PATH} &>/dev/null || true
+xargo build --release --target ${FULL_TOOLCHAIN}
+
+echo "Before Strip: $(ls -lh ${BINARY_PATH} | awk '{print $5}')"
+strip ${BINARY_PATH}
+echo " After Strip: $(ls -lh ${BINARY_PATH} | awk '{print $5}')"
+
+tar -zcf ${ZIP_PATH} ${BINARY_PATH}
+echo "GZipped File: $(ls -lh ${ZIP_PATH} | awk '{print $5}')"
+
+mv ${ZIP_PATH} ${FINAL_ZIP_PATH}
+
+echo "Built and zipped to ${FINAL_ZIP_PATH}"
