2b4fd6bb0c084a84d01f3434bc3b3114f89f5870
Dave Allie <dave@daveallie.com>
2018-05-17 23:31:53 +1000

    Use std as privnote data

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,32 @@ mod request;
 mod encryption;
 
 use rand::{thread_rng, Rng};
+use std::error::Error;
+use std::io::{self, Read};
 
 fn main() {
-    let password = generate_password();
-    let data = encryption::encrypt("test", &password).expect("FAILED");
-    let body = request::post_privnote_data(&data).expect("FAILED");
+    let data = get_data().expect("Failed to read data from stdin");
+    if data.is_empty() {
+        panic!("Data to be encrypted is empty");
+    }
 
-    let priv_res: PrivnoteRepsonse = serde_json::from_slice(body.as_bytes()).expect("FAILED");
+    let password = generate_password();
+    let enc_data = encryption::encrypt(&data, &password).expect("Failed to encrypt data");
+    let body = request::post_privnote_data(&enc_data).expect("Failed to post encrypted data to privnote");
+
+    let priv_res: PrivnoteRepsonse =
+        serde_json::from_slice(body.as_bytes()).expect("Failed to parse privnote response to JSON");
 
     println!("{}#{}", priv_res.note_link, password);
+}
+
+fn get_data() -> Result<String, Box<Error>> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+    Ok(buffer)
 }
 
 fn generate_password() -> String {
